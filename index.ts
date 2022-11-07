992c87004bd56b63e465e91ea6bdf92d5cc3466c
Sergey Bashkin <se.bashkin@ya.ru>
2022-11-07 11:20:32 +0400

    feat

--- index.ts
+++ index.ts
@@ -4,8 +4,14 @@ import cron from 'node-cron'
 import superagent from 'superagent'
 import env from './env';
 
+const path = require('path')
+
 const app = express()
 const PORT = env.PORT
+
+app.get("/", function(request, response){
+  response.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`)
